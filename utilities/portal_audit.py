f7d4575331a17e8cf8b34cfa243dd2b8cf8faca0
Cacilia Ewenz <cacilia.ewenz@internode.on.net>
2020-07-13 21:10:12 +0930

    got portal_audit.py working

--- utilities/portal_audit.py
+++ utilities/portal_audit.py
@@ -10,9 +10,12 @@ import dateutil
 import matplotlib.pyplot as plt
 import numpy
 import pylab
+import xlwt
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 # PFP modules
 if not os.path.exists("../scripts/"):
-    print "portal_audit: the scripts directory is missing"
+    print("portal_audit: the scripts directory is missing")
     sys.exit()
 # since the scripts directory is there, try importing the modules
 sys.path.append('../scripts')
@@ -29,14 +32,15 @@ def do_audit_analysis(base_path):
                  "end_date": datetime.datetime(2000,1,1,0,0)}
     n = 0
     for site in sites:
-        portal_dir = os.path.join(base_path, site, "Data", "Portal")
+        #portal_dir = os.path.join(base_path, site, "Data", "Processed")
+        portal_dir = os.path.join(base_path, site, "Data", "All")
         file_mask = os.path.join(portal_dir, "*.nc")
         files = glob.glob(file_mask)
-        l3_name = os.path.join(portal_dir, site + "_L3")
+        l3_name = os.path.join(portal_dir, site + "_L3.nc")
         if os.path.isfile(l3_name):
-            print "Processing ", site
+            print("Processing ", site)
             site_info[site] = {"file_name":l3_name}
-            ds = qcio.nc_read_series(l3_name)
+            ds = pfp_io.nc_read_series(l3_name)
             site_info[site]["site_name"] = ds.globalattributes["site_name"]
             start_date = dateutil.parser.parse(ds.globalattributes["start_date"])
             site_info[site]["start_date"] = start_date
@@ -53,7 +57,8 @@ def do_audit_analysis(base_path):
 
     return all_sites, site_info
 
-base_path = "/mnt/OzFlux/Sites/"
+#base_path = "/mnt/OzFlux/Sites/"
+base_path = "/run/media/cilli/ozflux2/OzFlux2/OzFlux_portal/Sites/"
 do_it = True
 
 if do_it:
@@ -63,9 +68,21 @@ else:
         l = pickle.load(handle)
         all_sites = l[0]
         site_info = l[1]
+# write to Excel file
+xl_file = xlwt.Workbook()
+xl_sheet = xl_file.add_sheet("Dates")
+xl_sheet.write(0,0,"Site")
+xl_sheet.write(0,1,"Start")
+xl_sheet.write(0,2,"End")
+for n,site in enumerate(sorted(list(site_info.keys()))):
+    xl_sheet.write(n+1,0,site_info[site]["site_name"])
+    xl_sheet.write(n+1,1,str(site_info[site]["start_date"]))
+    xl_sheet.write(n+1,2,str(site_info[site]["end_date"]))
+xl_file.save("portal_audit.xls")
+
 end_year = all_sites["end_date"].year
 all_sites["end_date"] = datetime.datetime(end_year+1, 1, 1, 0, 0)
-site_list = site_info.keys()
+site_list = list(site_info.keys())
 ylabel_list = [""]+site_list+[""]
 color_list = ["blue", "red", "green", "yellow", "magenta", "black", "orange", "brown"]
 xsize = 15.0
@@ -76,7 +93,7 @@ plt.ylim([0, len(site_list)+1])
 plt.xlim([all_sites["start_date"], all_sites["end_date"]])
 for n, site in enumerate(site_list):
     plt.plot(site_info[site]["X"], site_info[site]["Y"], color=color_list[numpy.mod(n,8)], linewidth=4)
-ylabel_posn = range(0, len(site_list)+2)
+ylabel_posn = list(range(0, len(site_list)+2))
 pylab.yticks(ylabel_posn, ylabel_list)
 fig.tight_layout()
 #plt.show()
@@ -91,10 +108,10 @@ plt.ylim([0, len(active_sites)+1])
 plt.xlim([all_sites["start_date"], all_sites["end_date"]])
 for n, site in enumerate(active_sites):
     plt.plot(site_info[site]["X"], [n+1, n+1], color=color_list[numpy.mod(n,8)], linewidth=4)
-ylabel_posn = range(0, len(active_sites)+2)
+ylabel_posn = list(range(0, len(active_sites)+2))
 pylab.yticks(ylabel_posn, ylabel_list)
 fig.tight_layout()
 
 plt.show()
 
-print "All done"
+print("All done")
